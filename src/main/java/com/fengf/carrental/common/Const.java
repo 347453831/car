5d44462d147d15785f421854b35c4375afdb5ae2
347453831 <56501949+347453831@users.noreply.github.com>
2020-11-26 22:19:55 +0800

    v1

--- src/main/java/com/fengf/carrental/common/Const.java
+++ src/main/java/com/fengf/carrental/common/Const.java
@@ -7,4 +7,5 @@ package com.fengf.carrental.common;
 public class Const {
     public static  final String CURRENT_USER="currentUser";
     public static  final String CURRENT_USER1="currentUser";
+    public static  final String CURRENT_USER2="currentUser";
 }
